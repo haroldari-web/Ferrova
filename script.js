2aab7426191f1d7c9bb909dec3ae19393732f906
haroldari-web <harold.ari@uab.edu.bo>
2026-08-04 21:58:01 -0400

    COMMIT 7
    Validación del formulario con JS.

--- script.js
+++ script.js
@@ -24,3 +24,28 @@ function alternarMenu() {
 }
 
 botonMenu.addEventListener("click", alternarMenu);
+
+// Avisos
+const formularioContacto = document.querySelector("#form-contacto");
+const avisoContacto = document.querySelector("#aviso-contacto");
+
+function revisarContacto(event) {
+    event.preventDefault();
+    const nombre = document.querySelector("#nombre").value;
+    const correo = document.querySelector("#correo").value;
+    if (nombre === "") {
+        avisoContacto.textContent = "Falta su nombre.";
+        avisoContacto.classList.add("error");
+        avisoContacto.classList.remove("exito");
+    } else if (correo.includes("@") === false) {
+        avisoContacto.textContent = "Por favor ingrese un correo valido.";
+        avisoContacto.classList.add("error");
+        avisoContacto.classList.remove("exito");
+    } else {
+        avisoContacto.textContent = "Solicitud enviada, usted sera contactado pronto.";
+        avisoContacto.classList.add("exito");
+        avisoContacto.classList.remove("error");
+    }
+}
+
+formularioContacto.addEventListener("submit", revisarContacto);
