2c7121db819d3d1bd7b272c0fdc319335933c2ed
haroldari-web <harold.ari@uab.edu.bo>
2026-08-18 19:42:41 -0400

    COMMIT 1
    Creacion del Proyecto en Laravel con su Model, su Migration y el Layout.

---
+++ integradora/resources/views/base.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Librería el Lápiz</title>
+</head>
+<body>
+    <header>
+        <h1>Librería el Lápiz</h1>
+    </header>
+
+    <main>
+        @yield('contenido')
+    </main>
+
+    <footer>
+        Integradora - Harold Rodrigo Ari Escobar - 18 de agosto de 2026
+    </footer>
+</body>
+</html>
